507c1ff5e61673a6b67af43393752c1b82211274
badamson1999 <61796259+badamson1999@users.noreply.github.com>
2020-12-10 15:06:37 -0600

    Update api.Defaults.java

---
+++ Homework2/api.Defaults.java
@@ -0,0 +1,12 @@
+package api;
+
+public class Defaults {
+
+	public static final int DEFAULT_BOWLS_PER_OVER = 6; //Default number of bowls per over.
+
+	public static final int DEFAULT_NUM_INNINGS = 2; //Default number of innings in the game.
+
+	public static final int DEFAULT_NUM_PLAYERS = 11; //Default number of players on each side.
+
+	public static final int DEFAULT_OVERS_PER_INNINGS = 50; //Default number of overs per innings.
+  }
